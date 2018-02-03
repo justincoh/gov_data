1f6cbd331fbe206fb31c8d3b550fbf123ddf37f1
Justin Cohen <justin.cohen@handshake.com>
2018-02-03 16:12:47 -0500

    adds generic ajax handler

--- main.py
+++ main.py
@@ -4,28 +4,41 @@ Main App file
 import csv
 import io
 import json
+import os
 import settings
 
-from flask import Flask, render_template
+from flask import Flask, render_template  # , request
 app = Flask(__name__)
 
-def read_csv(filename):
 
-    with io.open(filename, "r", encoding="utf-8-sig") as infile:
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+def read_csv(filename):
+    path = os.path.join(os.getcwd(), filename)
+    with io.open(path, "r", encoding="utf-8-sig") as infile:
         reader = csv.DictReader(infile)
         data = [row for row in reader]
         return data
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
 
-@app.route("/outlays")
-def outlays():
-    data = read_csv(settings.FILE_OUTLAY_BY_AGENCY)
-    return json.dumps(data)
+@app.route("/data/<file_constant>")
+def get_data(file_constant):
+    """
+    Ajax entry endpoint for csv data
+    TODO gate this?
+    """
+    try:
+        filename = getattr(settings, file_constant)
 
-@app.route("/testfile")
-def testfile():
-    data = read_csv(settings.FILE_TEST)
+    except AttributeError:
+        return app.response_class(
+            response="Invalid file name",
+            status=404,
+            mimetype="application/json",
+        )
+
+    data = read_csv(filename)
     return json.dumps(data)
